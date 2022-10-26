59d40a3caa93c10a474a8b2dc669b2ffdfb949d8
Edy Santosa Putra <edy.santosa.p@gmail.com>
2022-10-26 09:58:41 +0800

    Cleaning up

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -53,7 +53,7 @@
             <!-- Main Sidebar Container -->
             <aside class="main-sidebar sidebar-dark-primary elevation-4">
                 <!-- Brand Logo -->
-                <a href="../../index3.html" class="brand-link">
+                <a href="{{url('/')}}" class="brand-link">
                     <img src="{{ asset('dist/images/sikompak_logo.png') }}" alt="Logo Sikompak" class="brand-image img-circle elevation-3" style="opacity: .8">
                     <span class="brand-text font-weight-light">SIKOMPAK</span>
                 </a>
@@ -76,49 +76,11 @@
 
             <!-- Content Wrapper. Contains page content -->
             <div class="content-wrapper">
-                <!-- Content Header (Page header) -->
-                <section class="content-header">
-                    <div class="container-fluid">
-                        <div class="row mb-2">
-                            <div class="col-sm-6">
-                                <h1>Blank Page {{ Request::route()->uri()  }}</h1>
-                            </div>
-                            <div class="col-sm-6">
-                                <ol class="breadcrumb float-sm-right">
-                                    <li class="breadcrumb-item"><a href="#">Home</a></li>
-                                    <li class="breadcrumb-item active">Blank Page</li>
-                                </ol>
-                            </div>
-                        </div>
-                    </div><!-- /.container-fluid -->
-                </section>
+                @yield('contentheader')
 
                 <!-- Main content -->
                 <section class="content">
-                    <!-- Default box -->
-                    <div class="card">
-                        <div class="card-header">
-                            <h3 class="card-title">Title</h3>
-
-                            <div class="card-tools">
-                                <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-                                    <i class="fas fa-minus"></i>
-                                </button>
-                                <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-                                    <i class="fas fa-times"></i>
-                                </button>
-                            </div>
-                        </div>
-                        <div class="card-body">
-                            Start creating your amazing application!
-                        </div>
-                        <!-- /.card-body -->
-                        <div class="card-footer">
-                            Footer
-                        </div>
-                        <!-- /.card-footer-->
-                    </div>
-                    <!-- /.card -->
+                    @yield('content')
                 </section>
                 <!-- /.content -->
             </div>
