de1a4d2b4d2f68e4e5327f778611a218a0dc597c
Edy Santosa Putra <edy.santosa.p@gmail.com>
2022-11-05 11:14:53 +0800

    sdfsdfsdf

--- database/seeders/MenuSeeder.php
+++ database/seeders/MenuSeeder.php
@@ -15,15 +15,87 @@ class MenuSeeder extends Seeder
     public function run()
     {
         $menus = [
-            ['id' => '1', 'menu_title' => 'Dashboard', 'parent_id' => 0, 'sort_order' => 0, 'slug' => '/', 'icon' => 'fas fa-tachometer-alt'],
-            ['id' => '2', 'menu_title' => 'Pendataan PMKS', 'parent_id' => 0, 'sort_order' => 1, 'slug' => '/pmks', 'icon' => 'fas fa-address-card'],
-            ['id' => '3', 'menu_title' => 'Pendataan PSKS', 'parent_id' => 0, 'sort_order' => 2, 'slug' => '/psks', 'icon' => 'fas fa-hand-holding-heart'],
-            ['id' => '34', 'menu_title' => 'Komunikasi', 'parent_id' => 0, 'sort_order' => 3, 'slug' => '/komunikasi', 'icon' => 'fas fa-comments'],
-            ['id' => '10', 'menu_title' => 'Pengaturan', 'parent_id' => 0, 'sort_order' => 10, 'slug' => '', 'icon' => 'fas fa-cogs'],
-            ['id' => '11', 'menu_title' => 'User', 'parent_id' => 10, 'sort_order' => 1, 'slug' => '/admin/users', 'icon' => ''],
-            ['id' => '12', 'menu_title' => 'Submenu level 1', 'parent_id' => 10, 'sort_order' => 2, 'slug' => '/admin/users', 'icon' => ''],
-            ['id' => '13', 'menu_title' => 'Submenu level 2', 'parent_id' => 12, 'sort_order' => 1, 'slug' => '/admin/users', 'icon' => ''],
-            ['id' => '14', 'menu_title' => 'Submenu level 3', 'parent_id' => 12, 'sort_order' => 2, 'slug' => '/admin/users', 'icon' => ''],
+            [
+                'id' => '1',
+                'menu_title' => 'Dashboard',
+                'parent_id' => 0,
+                'sort_order' => 0,
+                'slug' => '/',
+                'icon' => 'fas fa-tachometer-alt',
+                'allowed_gates' => '["is-admin", "is-user"]'
+            ],
+            [
+                'id' => '2',
+                'menu_title' => 'Pendataan PMKS',
+                'parent_id' => 0,
+                'sort_order' => 1,
+                'slug' => '/pmks',
+                'icon' => 'fas fa-address-card',
+                'allowed_gates' => '["is-admin", "is-user"]'
+            ],
+            [
+                'id' => '3',
+                'menu_title' => 'Pendataan PSKS',
+                'parent_id' => 0,
+                'sort_order' => 2,
+                'slug' => '/psks',
+                'icon' => 'fas fa-hand-holding-heart',
+                'allowed_gates' => '["is-admin", "is-user"]'
+            ],
+            [
+                'id' => '4',
+                'menu_title' => 'Komunikasi',
+                'parent_id' => 0,
+                'sort_order' => 3,
+                'slug' => '/komunikasi',
+                'icon' => 'fas fa-comments',
+                'allowed_gates' => '["is-admin", "is-user"]'
+            ],
+            [
+                'id' => '10',
+                'menu_title' => 'Pengaturan',
+                'parent_id' => 0,
+                'sort_order' => 10,
+                'slug' => '',
+                'icon' => 'fas fa-cogs',
+                'allowed_gates' => '["is-admin"]'
+            ],
+            [
+                'id' => '11',
+                'menu_title' => 'User',
+                'parent_id' => 10,
+                'sort_order' => 1,
+                'slug' => '/admin/users',
+                'icon' => '',
+                'allowed_gates' => '["is-admin"]'
+            ],
+            [
+                'id' => '12',
+                'menu_title' => 'Submenu level 1',
+                'parent_id' => 10,
+                'sort_order' => 2,
+                'slug' => '/admin/sub1',
+                'icon' => '',
+                'allowed_gates' => '["is-admin"]'
+            ],
+            [
+                'id' => '13',
+                'menu_title' => 'Submenu level 2',
+                'parent_id' => 12,
+                'sort_order' => 1,
+                'slug' => '/admin/sub2',
+                'icon' => '',
+                'allowed_gates' => '["is-admin"]'
+            ],
+            [
+                'id' => '14',
+                'menu_title' => 'Submenu level 3',
+                'parent_id' => 12,
+                'sort_order' => 2,
+                'slug' => '/admin/sub3',
+                'icon' => '',
+                'allowed_gates' => '["is-admin"]'
+            ],
         ];
         foreach ($menus as $menu) {
             \App\Models\Menu::Create($menu);
