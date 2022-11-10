ed04ba65c7616a467ad4ff709b33a15b91f6c3b0
Edy Santosa Putra <edy.santosa.p@gmail.com>
2022-11-10 11:37:38 +0800

    Cleaning up

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -7,7 +7,7 @@
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h1>My User Profile</h1>
+                <h1>Profil Saya</h1>
             </div>
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
