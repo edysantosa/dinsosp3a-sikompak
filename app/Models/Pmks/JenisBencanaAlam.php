a2b0c2fc34e5bbed2f78cd2a081079b60dc8de70
Edy Santosa Putra <edy.santosa.p@gmail.com>
2022-11-14 10:11:10 +0800

    Jenis Bencana Alam

---
+++ app/Models/Pmks/JenisBencanaAlam.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\Pmks;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class JenisBencanaAlam extends Model
+{
+    use HasFactory;
+
+    protected $table = 'jenis_bencana_alam';
+}
---
+++ database/seeders/JenisBencanaAlamSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class JenisBencanaAlamSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $bencanas = [
+            [
+                'id' => '1',
+                'nama' => 'Disabilitas fisik',
+            ],
+            [
+                'id' => '2',
+                'nama' => 'Disabilitas intelektual',
+            ],
+            [
+                'id' => '3',
+                'nama' => 'Disabilitas mental',
+            ],
+            [
+                'id' => '4',
+                'nama' => 'Disabilitas netra',
+            ],
+            [
+                'id' => '5',
+                'nama' => 'Disabilitas rungu',
+            ],
+            [
+                'id' => '6',
+                'nama' => 'Disabilitas wicara',
+            ],
+        ];
+        foreach ($bencanas as $bencana) {
+            \App\Models\Pmks\JenisBencanaAlam::Create($bencana);
+        }
+    }
+}
