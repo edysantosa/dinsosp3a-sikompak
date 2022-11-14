80589b000ad166f762966d770b8a97aa11cfa14e
Edy Santosa Putra <edy.santosa.p@gmail.com>
2022-11-14 10:17:24 +0800

    Jenis Kekerasan

---
+++ app/Models/Pmks/JenisKekerasan.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\Pmks;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class JenisKekerasan extends Model
+{
+    use HasFactory;
+
+    protected $table = 'jenis_kekerasan';
+}
