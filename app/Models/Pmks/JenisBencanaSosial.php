f75789833f6bb1840b04befdf8a6276fec8d905b
Edy Santosa Putra <edy.santosa.p@gmail.com>
2022-11-14 09:35:26 +0800

    Jenis Bencana Sosial

---
+++ app/Models/Pmks/JenisBencanaSosial.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\Pmks;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class JenisBencanaSosial extends Model
+{
+    use HasFactory;
+
+    protected $table = 'jenis_bencana_sosial';
+}
