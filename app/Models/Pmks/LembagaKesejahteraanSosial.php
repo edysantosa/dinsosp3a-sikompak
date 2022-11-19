617ef752bdd26608c88cd84ec43fcf10d52c7815
Edy Santosa Putra <edy.santosa.p@gmail.com>
2022-11-19 11:28:16 +0800

    lks

--- app/Models/Pmks/LembagaKesejahteraanSosial.php
+++ app/Models/Pmks/LembagaKesejahteraanSosial.php
@@ -10,4 +10,25 @@ class LembagaKesejahteraanSosial extends Model
     use HasFactory;
 
     protected $table = 'lembaga_kesejahteraan_sosial';
+
+
+    public function provinsi()
+    {
+        return $this->belongsTo('App\Models\Provinsi');
+    }
+
+    public function kabupatenKota()
+    {
+        return $this->belongsTo('App\Models\KabupatenKota');
+    }
+
+    public function kecamatan()
+    {
+        return $this->belongsTo('App\Models\Kecamatan');
+    }
+
+    public function kelurahan()
+    {
+        return $this->belongsTo('App\Models\Kelurahan');
+    }
 }
