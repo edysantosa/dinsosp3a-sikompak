3c1e4a6c08b1829de300ae889972d49de7c40e76
Edy Santosa Putra <celak.pande@gmail.com>
2022-12-17 12:31:21 +0800

    Factory

--- database/factories/Pmks/PmksFactory.php
+++ database/factories/Pmks/PmksFactory.php
@@ -74,81 +74,90 @@ class PmksFactory extends Factory
         return $this->afterCreating(function (Pmks $pmks) {
             //Jumlah jenis masalah sosial yang dimiliki oleh PMKS 
             //sebagai simulasi PMKS bisa memiliki lebih dari satu masalah sosial (max 3)
-            // $jumlahPMKS = $this->faker->biasedNumberBetween(0, 3, 'linearLow');
-            
-            $jenisPmks = JenisPmks::all()->random();
-            $additionalData = [];
-            $isDalamPanti = $this->faker->randomElement([0, 1]);
+            $jumlahPMKS = $this->faker->biasedNumberBetween(1, 3, 'Faker\Provider\Biased::linearLow');
 
-            switch ($jenisPmks->id) {
-                case JenisPmks::KORBAN_KEKERASAN:
-                case JenisPmks::ANAK_PERLU_PERLINDUNGAN:
-                case JenisPmks::ANAK_JALANAN:
-                case JenisPmks::PENYANDANG_DISABILITAS:
-                case JenisPmks::TERLANTAR:
-                    if ($isDalamPanti) {
+            $arrAdditionalData = [];
+            for ($i=0; $i < $jumlahPMKS; $i++) { 
+                $jenisPmks = JenisPmks::all()->random();
+                $existingJenis = \App\Models\Pmks\JenisPmksPmks::where('pmks_id', 100)->get()->pluck('jenis_pmks_id')->toArray();
+                if (in_array($jenisPmks->id, $existingJenis)) {
+                    break;
+                }
+
+
+                $additionalData = [];
+                $isDalamPanti = $this->faker->randomElement([0, 1]);
+
+                switch ($jenisPmks->id) {
+                    case JenisPmks::KORBAN_KEKERASAN:
+                    case JenisPmks::ANAK_PERLU_PERLINDUNGAN:
+                    case JenisPmks::ANAK_JALANAN:
+                    case JenisPmks::PENYANDANG_DISABILITAS:
+                    case JenisPmks::TERLANTAR:
+                        if ($isDalamPanti) {
+                            $additionalData = [
+                                'lembaga_kesejahteraan_sosial_id' => LembagaKesejahteraanSosial::all()->random()->id
+                            ];
+                        } else {
+                            $additionalData = [
+                                'nama_keluarga' => $this->faker->name($this->faker->randomElement(['male', 'female'])),
+                                'hubungan_keluarga' => $this->keluarga($pmks),
+                            ];
+                        }
+                        switch ($jenisPmks->id) {
+                            case JenisPmks::PENYANDANG_DISABILITAS:
+                                $additionalData['jenis_disabilitas_id'] = JenisDisabilitas::all()->random()->id;
+                                break;
+                            case JenisPmks::KORBAN_KEKERASAN:
+                                $additionalData['jenis_kekerasan_id'] = JenisKekerasan::all()->random()->id;
+                                break;
+                        }
+                        break;
+
+
+                    case JenisPmks::GELANDANGAN:
+                    case JenisPmks::PENGEMIS:
+                        if ($isDalamPanti) {
+                            $additionalData = [
+                                'lembaga_kesejahteraan_sosial_id' => LembagaKesejahteraanSosial::all()->random()->id
+                            ];
+                        }
+                        break;
+
+
+                    case JenisPmks::KORBAN_BENCANA_ALAM:
+                    case JenisPmks::KORBAN_BENCANA_SOSIAL:
                         $additionalData = [
-                            'lembaga_kesejahteraan_sosial_id' => LembagaKesejahteraanSosial::all()->random()->id
+                            'tanggal_bencana' => $this->faker->dateTimeBetween($startDate = '-24 months', $endDate = 'now')->format("Y-m-d"),
+                            'jumlah_korban' => $this->faker->randomNumber(4, false),
                         ];
-                    } else {
+                        switch ($jenisPmks->nama) {
+                            case JenisPmks::KORBAN_BENCANA_ALAM:
+                                $additionalData['jenis_bencana_alam_id'] = JenisBencanaAlam::all()->random()->id;
+                                break;
+                            case JenisPmks::KORBAN_BENCANA_SOSIAL:
+                                $additionalData['jenis_bencana_sosial_id'] = JenisBencanaSosial::all()->random()->id;
+                                break;
+                        }
+                        break;
+
+
+                    case JenisPmks::KOMUNITAS_ADAT_TERPENCIL:
                         $additionalData = [
-                            'nama_keluarga' => $this->faker->name($this->faker->randomElement(['male', 'female'])),
-                            'hubungan_keluarga' => $this->keluarga($pmks),
+                            'jumlah_laki' => $this->faker->randomNumber(4, false),
+                            'jumlah_perempuan' => $this->faker->randomNumber(4, false),
                         ];
-                    }
-                    switch ($jenisPmks->id) {
-                        case JenisPmks::PENYANDANG_DISABILITAS:
-                            $additionalData['jenis_disabilitas_id'] = JenisDisabilitas::all()->random()->id;
-                            break;
-                        case JenisPmks::KORBAN_KEKERASAN:
-                            $additionalData['jenis_kekerasan_id'] = JenisKekerasan::all()->random()->id;
-                            break;
-                    }
-                    break;
+                        break;
 
-
-                case JenisPmks::GELANDANGAN:
-                case JenisPmks::PENGEMIS:
-                    if ($isDalamPanti) {
+                    case JenisPmks::ANAK_BERHADAPAN_HUKUM:
                         $additionalData = [
-                            'lembaga_kesejahteraan_sosial_id' => LembagaKesejahteraanSosial::all()->random()->id
+                            'status_hukum' => $this->faker->randomElement(['Tersangka', 'Pelaku', 'Tahanan', 'Narapidana']),
                         ];
-                    }
-                    break;
-
-
-                case JenisPmks::KORBAN_BENCANA_ALAM:
-                case JenisPmks::KORBAN_BENCANA_SOSIAL:
-                    $additionalData = [
-                        'tanggal_bencana' => $this->faker->dateTimeBetween($startDate = '-24 months', $endDate = 'now')->format("Y-m-d"),
-                        'jumlah_korban' => $this->faker->randomNumber(4, false),
-                    ];
-                    switch ($jenisPmks->nama) {
-                        case JenisPmks::KORBAN_BENCANA_ALAM:
-                            $additionalData['jenis_bencana_alam_id'] = JenisBencanaAlam::all()->random()->id;
-                            break;
-                        case JenisPmks::KORBAN_BENCANA_SOSIAL:
-                            $additionalData['jenis_bencana_sosial_id'] = JenisBencanaSosial::all()->random()->id;
-                            break;
-                    }
-                    break;
-
-
-                case JenisPmks::KOMUNITAS_ADAT_TERPENCIL:
-                    $additionalData = [
-                        'jumlah_laki' => $this->faker->randomNumber(4, false),
-                        'jumlah_perempuan' => $this->faker->randomNumber(4, false),
-                    ];
-                    break;
-
-                case JenisPmks::ANAK_BERHADAPAN_HUKUM:
-                    $additionalData = [
-                        'status_hukum' => $this->faker->randomElement(['Tersangka', 'Pelaku', 'Tahanan', 'Narapidana']),
-                    ];
-                    break;
+                        break;
+                }
+                $arrAdditionalData[$jenisPmks->id] = $additionalData;
             }
-
-            $pmks->jenisPmks()->attach($jenisPmks->id, $additionalData);
+            $pmks->jenisPmks()->sync($arrAdditionalData);
         });
     }
 
